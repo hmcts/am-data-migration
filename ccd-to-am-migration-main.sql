b25305af3939721e74e9cb793d0d505a5e8a998f
Ben Matthews <benjamin.matthews@hmcts.net>
2019-06-19 14:47:12 +0100

    update migration script to avoid unnecessary tables

--- ccd-to-am-migration-main.sql
+++ ccd-to-am-migration-main.sql
@@ -39,15 +39,11 @@ INSERT INTO access_management (resource_id, accessor_type, accessor_id,
         "attribute", permissions, service_name, resource_name,
         resource_type, relationship)
     SELECT s.case_data_id AS resource_id, 'USER' AS accessor_type,
-        s.user_id AS accessor_id, ra."attribute" AS "attribute",
-        dp.permissions AS permissions, re.service_name AS service_name,
-        s.case_type_id AS resource_name, re.resource_type AS resource_type,
-        s.case_role AS relationship
-    FROM stage AS s, resource_attributes AS ra,
-        default_permissions_for_roles AS dp, resources AS re
-    WHERE ra.resource_name = s.case_type_id
-    AND dp.resource_name = s.case_type_id
-    AND re.resource_name = s.case_type_id
+        s.user_id AS accessor_id, '' AS "attribute", 0 AS permissions,
+        r.service_name AS service_name, s.case_type_id AS resource_name,
+        r.resource_type AS resource_type, s.case_role AS relationship
+    FROM stage AS s, resources AS r
+    WHERE r.resource_name = s.case_type_id
 EXCEPT
     SELECT resource_id, accessor_type, accessor_id, "attribute",
         permissions, service_name, resource_name, resource_type,

72fa7678046443d0a574329ac698782b7d745aa1
Ben Matthews <benjamin.matthews@hmcts.net>
2019-06-25 15:37:19 +0100

    write log to file

--- am-migration-runner.sh
+++ am-migration-runner.sh
@@ -28,7 +28,7 @@ echo "* AM DB name: $DB"
 echo "* AM DB username: $USER"
 echo
 
-$(psql \
+METRICS=$(psql \
     -X \
     -q \
     -h $HOST \
@@ -38,7 +38,9 @@ $(psql \
     --set AUTOCOMMIT=off \
     --set ON_ERROR_ROLLBACK=on \
     --set ON_ERROR_STOP=off \
-    $DB) > output.o
+    $DB)
+
+echo "$METRICS" > output.o
 
 echo "* Migration finished"
 echo
